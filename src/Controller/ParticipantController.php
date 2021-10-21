50cbe689b0394a6ea5f8dfce5cdde72a3f0be02d
SebastienBonneau <sebastien.s.bonneau@gmail.com>
2021-10-21 09:58:40 +0200

    hierarchy plus protection route en fonction des doles

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -11,7 +11,7 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/Participant", name="participant")
+ * @Route("/participant", name="participant")
  */
 class ParticipantController extends AbstractController
 {
